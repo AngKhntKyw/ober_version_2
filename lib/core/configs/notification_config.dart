cfa050fd24fc9bcce366d97034ba44161c60eb5c
Aung Khant <aungkhantkyaw8918@gmail.com>
2025-01-27 16:59:31 +0630

    update noti

--- lib/core/configs/notification_config.dart
+++ lib/core/configs/notification_config.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
 import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 FirebaseMessaging messaging = FirebaseMessaging.instance;
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -19,6 +21,10 @@ void initNoti() async {
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
 //
+  if (await Permission.notification.status == PermissionStatus.denied) {
+    await Permission.notification.request();
+  }
+
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
     announcement: false,
@@ -28,69 +34,15 @@ void initNoti() async {
     provisional: false,
     sound: true,
   );
-
 //
-
-  Future<void> showNoti({
-    required String title,
-    required String body,
-    required String imageUrl,
-  }) async {
-    String? localImagePath;
-    if (imageUrl.isNotEmpty) {
-      try {
-        final http.Response response = await http.get(Uri.parse(imageUrl));
-        final Directory directory = await getApplicationDocumentsDirectory();
-        final File file = File('${directory.path}/notification_image.jpg');
-        await file.writeAsBytes(response.bodyBytes);
-        localImagePath = file.path;
-      } catch (e) {
-        log('Error downloading image: $e');
-      }
-    }
-
-    final BigPictureStyleInformation bigPictureStyleInformation =
-        localImagePath != null
-            ? BigPictureStyleInformation(
-                FilePathAndroidBitmap(localImagePath),
-                contentTitle: title,
-                summaryText: body,
-                hideExpandedLargeIcon: false,
-              )
-            : BigPictureStyleInformation(
-                const DrawableResourceAndroidBitmap('app_icon'),
-                contentTitle: title,
-                summaryText: body,
-                hideExpandedLargeIcon: false,
-              );
-
-    final AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      'high_importance_channel',
-      'High Importance Notifications',
-      channelDescription: 'This channel is used for important notifications.',
-      styleInformation: bigPictureStyleInformation,
-      importance: Importance.high,
-      priority: Priority.high,
-      largeIcon: FilePathAndroidBitmap(localImagePath!),
-    );
-
-    NotificationDetails notificationDetails =
-        NotificationDetails(android: androidNotificationDetails);
-
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      title,
-      body,
-      notificationDetails,
-    );
-  }
 
   log("Setting authorization status: ${settings.authorizationStatus}");
   if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    // on message
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      log("event : ${message.notification!.android!.imageUrl}");
       if (message.notification != null) {
+        // Extract data payload
+
         await showNoti(
           title: message.notification!.title ?? "",
           body: message.notification!.body ?? "",
@@ -98,5 +50,78 @@ void initNoti() async {
         );
       }
     });
+
+    // background message
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   }
+}
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (message.notification != null) {
+    // Extract data payload
+    final String title = message.data['title'] ?? "";
+    final String body = message.data['body'] ?? "";
+    final String imageUrl = message.data['imageUrl'] ?? "";
+
+    await showNoti(title: title, body: body, imageUrl: imageUrl);
+  }
+}
+
+Future<void> showNoti({
+  required String title,
+  required String body,
+  required String imageUrl,
+}) async {
+  String? localImagePath;
+  if (imageUrl.isNotEmpty) {
+    try {
+      final http.Response response = await http.get(Uri.parse(imageUrl));
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final File file = File('${directory.path}/notification_image.jpg');
+      await file.writeAsBytes(response.bodyBytes);
+      localImagePath = file.path;
+    } catch (e) {
+      log('Error downloading noti image: $e');
+    }
+  }
+
+  final BigPictureStyleInformation bigPictureStyleInformation =
+      localImagePath != null
+          ? BigPictureStyleInformation(
+              FilePathAndroidBitmap(localImagePath),
+              contentTitle: title,
+              summaryText: body,
+              hideExpandedLargeIcon: false,
+            )
+          : BigPictureStyleInformation(
+              const DrawableResourceAndroidBitmap('app_icon'),
+              contentTitle: title,
+              summaryText: body,
+              hideExpandedLargeIcon: false,
+            );
+
+  final AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+    'high_importance_channel',
+    'High Importance Notifications',
+    channelDescription: 'This channel is used for important notifications.',
+    styleInformation: bigPictureStyleInformation,
+    importance: Importance.high,
+    priority: Priority.high,
+    largeIcon: FilePathAndroidBitmap(localImagePath!),
+    channelShowBadge: true,
+  );
+
+  NotificationDetails notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    title,
+    body,
+    notificationDetails,
+  );
 }
